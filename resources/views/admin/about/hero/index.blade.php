4a1a2473dd8912f2ee6593bf520fc2ba2c7b0aa8
Jitendrapranto <jitendrapranto@gmail.com>
2025-12-06 17:48:38 +0600

    fix: Remove unnecessary whitespace in Submit Button section of About Hero Section form

--- resources/views/admin/about/hero/index.blade.php
+++ resources/views/admin/about/hero/index.blade.php
@@ -100,7 +100,7 @@
             <!-- Submit Button -->
             <div class="flex justify-end pt-4">
                 <button type="submit" class="px-6 py-2 bg-blue-600 text-white rounded-sm hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-                    Update 
+                    Update
                 </button>
             </div>
         </form>
