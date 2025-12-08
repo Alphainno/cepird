eb151b3da2e722a162f33fc38d1274ed4fee7763
Jitendrapranto <jitendrapranto@gmail.com>
2025-12-08 17:50:13 +0600

    feat: reorganize database seeders for improved structure and clarity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,34 +33,51 @@ class DatabaseSeeder extends Seeder
 
         // Seed Hero Section
         $this->call([
+            // Landing Page
             HeroSectionSeeder::class,
+            
+            // About Page
             AboutSectionSeeder::class,
             AboutHeroSectionSeeder::class,
             AboutIntroductionSectionSeeder::class,
             VisionMissionSectionSeeder::class,
             CoreValueSeeder::class,
             WhatWeDoSeeder::class,
-            ProgramInitiativeSeeder::class,
-            FocusAreaSeeder::class,
-            VisionSectionSeeder::class,
-            ProgramSeeder::class,
             FounderSeeder::class,
-            CtaSectionSeeder::class,
+            
+            // Focus Areas Page
+            FocusAreaSeeder::class,
             FocusAreaHeroSectionSeeder::class,
             StrategicPillarSeeder::class,
             FocusAreaOutcomeSeeder::class,
             FocusAreaOutcomeSectionSeeder::class,
-            ImpactMetricSeeder::class,
-            ImpactSectionSeeder::class,
             FocusAreaCtaSectionSeeder::class,
+            
+            // Programs Page
+            ProgramInitiativeSeeder::class,
+            VisionSectionSeeder::class,
+            ProgramSeeder::class,
+            ProgramHeroSectionSeeder::class,
+            ProgramOverviewSectionSeeder::class,
+            ProgramCategorySeeder::class,
+            ProgramSectionSeeder::class,
+            ProgramImpactSeeder::class,
+            
+            // Research Page
+            ResearchHeroSectionSeeder::class,
+            ResearchCategorySeeder::class,
+            ResearchPaperSeeder::class,
+            
+            // Contact Page
             ContactHeroSectionSeeder::class,
             ContactInfoSectionSeeder::class,
             ContactMapSectionSeeder::class,
             ContactCtaSectionSeeder::class,
-            ProgramSectionSeeder::class,
-            ResearchHeroSectionSeeder::class,
-            ResearchCategorySeeder::class,
-            ResearchPaperSeeder::class,
+            
+            // Shared Components
+            CtaSectionSeeder::class,
+            ImpactMetricSeeder::class,
+            ImpactSectionSeeder::class,
             HeaderSeeder::class,
             FooterSeeder::class,
             
